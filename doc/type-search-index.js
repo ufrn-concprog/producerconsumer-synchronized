4e63b5fda85ff3ba6ab5bb0dc3fc6d01925dffd7
Everton Cavalcante <evertonranielly@gmail.com>
2022-10-21 19:25:20 -0300

    Adding documentation

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Consumer"},{"p":"<Unnamed>","l":"Producer"},{"p":"<Unnamed>","l":"ProducerConsumerMain"},{"p":"<Unnamed>","l":"SharedBuffer"}];updateSearchResults();
